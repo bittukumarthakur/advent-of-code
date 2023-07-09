72fab8cf6186268a9becd29c745bfa660c9a631b
Bittu <bittukumarthakur@users.noreply.github.com>
2023-07-09 18:46:49 +0530

    Day-4, first part completed.

--- 2015/day-4/main.js
+++ 2015/day-4/main.js
@@ -4,14 +4,14 @@ const { getSecretNumber } = require("./src/get-secrect-number");
 const main = () => {
 
   // first part
-  const firsttKey = "ckczppom";
-  const firstSecretNumber = getSecretNumber(firsttKey, "00000");
-  console.log("first part:", firstSecretNumber);
+  // const firsttKey = "ckczppom";
+  // const firstSecretNumber = getSecretNumber(firsttKey, "00000");
+  // console.log("first part:", firstSecretNumber);
 
   // second part
-  const secondKey = "ckczppom";
-  const secondSecretNumber = getSecretNumber(secondKey, "000000");
-  console.log("second part", secondSecretNumber);
+  // const secondKey = "ckczppom";
+  // const secondSecretNumber = getSecretNumber(secondKey, "000000");
+  // console.log("second part", secondSecretNumber);
 };
 
 main();
